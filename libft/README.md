0966091d668dbaccdc6a1bb666c256ad8d922c9e
CroWcbr <crowf4i@google.com>
2022-12-23 01:37:58 +0300

    del obj files

--- libft/README.md
+++
@@ -1,6 +0,0 @@
-# ft_ssl_md5
-
-my PC has LITTLE-endian format
-
-in md5 - just append
-in shaXXX - move bits
--- libft/testbyteorder/testbyteorder.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-int main ()
-{
-  uint16_t x = 0x0001;
-  printf("%s-endian\n", *((uint8_t *) &x) ? "little" : "big");
-}
