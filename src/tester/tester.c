7466380a969105b92bf670e02808c877c27051a1
Vitaliy Shavnin <crowf4i@gmail.com>
2023-02-25 21:09:23 +0300

    tester

--- src/tester/tester.c
+++ src/tester/tester.c
@@ -33,7 +33,6 @@ static void	tester(char *hash, int hash_len, int test, int print, \
 	{
 		char random = rand() % 94 + 32;
 		str[i] = random;
-		// str[i] = '1';
 		openssl_hash(str, strlen(str), str_openssl);
 		str_my = my_hash(str, strlen(str));
 
@@ -55,10 +54,66 @@ static void	tester(char *hash, int hash_len, int test, int print, \
 			if (str_openssl[j] != str_my[j])
 			{
 				err++;
-				break;
-				// exit(0);
+				if (print == 0)
+					break;
+				else
+					exit(0);
 			}
 		free(str_my);
+	}
+	printf("-------------------------------------\n");
+	printf("\tError in test %s: %d\n", hash, err);
+	printf("-------------------------------------\n");
+}
+
+static void	tester_full_random(char *hash, int hash_len, int test, int str_len_test, int print, \
+					unsigned char * (*openssl_hash)(const unsigned char *, size_t,  unsigned char *), \
+					uint8_t* (*my_hash)(const char *, const uint64_t))
+{
+	int			err = 0;
+	char		*str;
+	uint8_t		str_openssl[hash_len];
+	uint8_t		*str_my;
+
+	for (int i = 0; i < test; i++)
+	{
+		int random = rand() % str_len_test;
+		str = (char*)malloc((random + 1)*sizeof(char));
+		str[random] = 0;
+		for (int j = 0; j < random; j++)
+		{
+			char random_char = rand() % 94 + 32;
+			str[j] = random_char;
+		}
+
+		openssl_hash(str, strlen(str), str_openssl);
+		str_my = my_hash(str, strlen(str));
+		if (print == 1)
+		{
+			printf("MD5 openssl: \t");
+			for(int i = 0; i < hash_len; i++)
+				printf("%02x", str_openssl[i]);
+			printf("\n");
+
+			printf("MD5 my func: \t");
+			for(int i = 0; i < hash_len; i++)
+				printf("%02x", str_my[i]);
+			printf("\n");
+		}
+		for (int j = 0; j < hash_len; j++)
+			if (str_openssl[j] != str_my[j])
+			{
+				err++;
+				if (print == 0)
+					break;
+				else
+				{
+					printf("%d\t|%s|\n", i, str);
+					exit(0);
+				}
+			}
+		free(str_my);
+		free(str);
 	}
 	printf("-------------------------------------\n");
 	printf("\tError in test %s: %d\n", hash, err);
@@ -68,12 +123,18 @@ static void	tester(char *hash, int hash_len, int test, int print, \
 int main()
 {
 	int test = 100;
+	int str_len_test = 100000;
 	int print = 0;
 	srand(time(NULL));
 	tester("md5", MD5_DIGEST_LENGTH, test, print, MD5, md5_main);
 	tester("sha256", SHA256_DIGEST_LENGTH, test, print, SHA256, sha256_main);
 	tester("sha512", SHA512_DIGEST_LENGTH, test, print, SHA512, sha512_main);
 	tester("whirlpool", WHIRLPOOL_DIGEST_LENGTH, test, print, WHIRLPOOL_EDIT, whirlpool);
+
+	tester_full_random("md5", MD5_DIGEST_LENGTH, test, str_len_test, print, MD5, md5_main);
+	tester_full_random("sha256", SHA256_DIGEST_LENGTH, test, str_len_test, print, SHA256, sha256_main);
+	tester_full_random("sha512", SHA512_DIGEST_LENGTH, test, str_len_test, print, SHA512, sha512_main);
+	tester_full_random("whirlpool", WHIRLPOOL_DIGEST_LENGTH, test, str_len_test, print, WHIRLPOOL_EDIT, whirlpool);
 
 	return (0);
 }
